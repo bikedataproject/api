cb9ce4f0cdd493cb986b3ff6ac8667fbcc42439f
Richez Antoine <richezatr@gmail.com>
2020-07-07 16:00:27 +0200

    Main architecture + sample core  objects

---
+++ src/BikeDataProject.Core/Constants/SampleConstantsFields.cs
@@ -0,0 +1,15 @@
+﻿namespace BikeDataProject.Core.Constants
+{
+    using System;
+
+    /// <summary>
+    /// Contains the sample constants.
+    /// </summary>
+    public static class SampleConstantsFields
+    {
+        /// <summary>
+        /// The soft version
+        /// </summary>
+        public const String SoftVersion = "1.1.3";
+    }
+}
---
+++ src/BikeDataProject.Core/Enums/SampleEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace BikeDataProject.Core.Enums
+{
+    /// <summary>
+    /// Contains the Sample enumeration.
+    /// </summary>
+    public enum SampleEnum
+    {
+        None = 0x0,
+        User = 0x2,
+        VerifiedUser = 0x4,
+        Admin = User | VerifiedUser | 0x8,
+        SuperAdmin = Admin | 0x16
+    }
+}
